5f37f6f66ed393c6a9421cdddd55f3a10adc93fd
joshua-harmon <joshua.harmon1960@gmail.com>
2020-10-26 20:03:56 -0700

    edits

--- src/components/Helloworld.vue
+++ src/components/Helloworld.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
 
     <h1 style = "color:blue; text-align:center">WORLD OF WORK SERVICES COOP</h1>
     <h2>sales</h2>
@@ -48,30 +45,4 @@ export default {
   }
 }
 </script>
-</template>
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
