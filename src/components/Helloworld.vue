4a05576a8582b5d27d18a8d738690894d5fc9797
joshua-harmon <joshua.harmon1960@gmail.com>
2020-11-14 13:00:40 -0700

    edits

--- src/components/Helloworld.vue
+++ src/components/Helloworld.vue
@@ -1,19 +1,18 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-
-    <h1 style = "color:blue; text-align:center">WORLD OF WORK SERVICES COOP</h1>
-    <h2>sales</h2>
-    <h2>construction management and all engineering</h2>
-    <h2>entry level</h2>
-    <h2>merit advancement</h2>
-    <h2>professional positions based upon experience and education</h2>
-    <h2>self-employment can immediately become part of the coop and can make money</h2>
-    <h2>employment-work options</h2>
+    <h1 style = "color:blue; text-align:center">WORLD OF WORK SERVICES - COOP</h1>
+    <h2>Working with employment opportunities in:</h2>
+    <h2>Sales</h2>  
+    <h2>Engineering (all disciplines)</h2>
+    <h2>Management (all categories)</h2>
+    <h2>Construction Management</h2>
+    <h2>Entry level employment</h2>
+    <h2>Merit advancement</h2>
+    <h2>Professional positions based upon experience and education</h2>
+    <h2>Self-employment by becomming a working COOP member</h2>
+    <h3>CALL (520) 300-6340<br>nk@world-of-work-services-coop.com<br>Josh Harmon<br>141 S. 6th Ave, Unit 42<br>Tucson, Az 85702</h3>
 <form name="contact" method="POST" data-netlify="true">
-  <p>
-    <label>Your Name: <input type="text" name="name" size="50" /></label>   
-  </p>
   <p>
     <label>Your Email: <input type="email" name="email" size="50" /></label>
   </p>
@@ -45,4 +44,33 @@ export default {
   }
 }
 </script>
+</template>
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
+
+
 
