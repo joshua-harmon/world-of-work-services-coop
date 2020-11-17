2f6dba6d8b72b2b1551df419adbfeb8f1a8907dc
joshua-harmon <joshua.harmon1960@gmail.com>
2020-11-17 09:53:42 -0700

    edits

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,11 +11,8 @@
     <h2>Merit advancement</h2>
     <h2>Professional positions based upon experience and education</h2>
     <h2>Self-employment by becoming a working COOP member</h2>
-    <h2>CALL (520) 300-6340</h2>
-    <h2>nk@World-of-work-services-coop.com</h2>
-    <h3>Josh Harmon</h3>
-    <h3>141 S. 6th Ave, Unit 42</h3>
-    <h3>Tucson, Az 85702</h3>
+    <h3>CALL (520) 300-6340<br>nk@World-of-work-services-coop.com</h3>
+    <h3>Kay or Josh Harmon<br>141 S. 6th Ave, Unit 42<br>Tucson, Az 85702</h3>
 
     <h3>Essential Links</h3>
     <ul>
