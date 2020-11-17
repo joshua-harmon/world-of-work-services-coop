2f2ec262e1be43225731301c41f6d2d0a694d6cf
joshua-harmon <joshua.harmon1960@gmail.com>
2020-11-17 05:05:47 -0700

    edits

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,46 +2,31 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-    <h1 style = "color:blue; text-align:center">WORLD OF WORK SERVICES - COOP</h1>
-    <h2>Working with employment opportunities in:</h2>
-    <h2>Sales</h2>  
-    <h2>Engineering (all disciplines)</h2>
-    <h2>Management (all categories)</h2>
-    <h2>Construction Management</h2>
-    <h2>Entry level employment</h2>
-    <h2>Merit advancement</h2>
-    <h2>Professional positions based upon experience and education</h2>
-    <h2>Self-employment by becoming a working COOP member</h2>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
-    <p>
-    <h3>CALL (520) 300-6340, nk@World-of-work-services-coop.com</h3>
-    <h3>Josh Harmon, 141 S. 6th Ave, Unit 42, Tucson, Az 85702</h3>
-    </p>
-<form name="contact" method="POST" data-netlify="true">
-  <p>
-    <label>Your Name: <input type="text" name="name" size="50" /></label>   
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" size="50" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="entry-level">entry level</option>
-      <option value="sales">sales</option>
-      <option value="software">software</option>
-      <option value="hardware">hardware</option>
-      <option value="manufacturing">manufacturing</option>
-      <option value="construction">construction</option>
-      <option value="other">other</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message" rows="8" columns=10></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
+    <h3>Installed CLI Plugins</h3>
+    <ul>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+    </ul>
+    <h3>Essential Links</h3>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
+      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+    </ul>
+    <h3>Ecosystem</h3>
+    <ul>
+      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
+      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
+      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
+      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+    </ul>
   </div>
 </template>
 
@@ -54,6 +39,7 @@ export default {
 }
 </script>
 
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
